2efdaf1d3492000fec4b46906ea5fbbc07782d49
Darshan667 <72382788+Darshan667@users.noreply.github.com>
2022-08-26 23:52:51 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,27 @@
+public class Variable {
+    public static void main(String[] args){
+        int a = 5;
+        a++;
+        byte b1 = 127;
+        short s1 = 8786;
+        long l1 = 4646466433333333333l;
+        double d = 6.7;
+        float f = 6.5f;
+        char c = 'N';
+        boolean b = true;
+
+        int num1 = 18;
+        int num2 = 7;
+        double result = (double)num1 % num2;
+        System.out.println(result);
+        System.out.println(a);
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(l1);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(f);
+
+    }
+}
